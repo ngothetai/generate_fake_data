07026aee1305a237581012365c72e17e94333518
taingoInfCapital <taingo@infcapital.vn>
2024-04-20 13:13:27 +0700

    change try catch

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 import pandas as pd
 import numpy as np
 
@@ -197,7 +199,25 @@ def convert_list_to_str(df):
     return df
 
 
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+
 def app():
+    print(f"The remote ip is {get_remote_ip()}")
     data = init_db()
     st.header("Ứng dụng tạo dữ liệu giả lập")
     
